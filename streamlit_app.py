c2f3240945d913b4d5bdbfce6e6705000a0bfd6b
Zaccaria <zaccaria.amillou@gmail.com>
2024-05-21 09:26:29 +0200

    adding pythonanywhere site to streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ id = st.slider('Choose an image id', min_value=1, max_value=21)
 
 if st.button('Submit'):
     # Make a POST request to the Flask app to process the image
-    response = requests.post(f'http://127.0.0.1:5000/prediction/', data={'file': id})
+    response = requests.post(f'https://zacam.pythonanywhere.com/prediction/', data={'file': id})
 
     # Check if the request was successful
     if response.status_code == 200:
@@ -21,16 +21,16 @@ if st.button('Submit'):
         col1, col2 = st.columns(2)
 
         # Display the real image and mask
-        response = requests.get(f'http://127.0.0.1:5000/static/data/predict/img.png')
+        response = requests.get(f'https://zacam.pythonanywhere.com/static/data/predict/img.png')
         image = Image.open(io.BytesIO(response.content))
         col1.image(image, caption='Real Image', use_column_width=True)
 
-        response = requests.get(f'http://127.0.0.1:5000/static/data/predict/mask.png')
+        response = requests.get(f'https://zacam.pythonanywhere.com/static/data/predict/mask.png')
         image = Image.open(io.BytesIO(response.content))
         col2.image(image, caption='Real Mask', use_column_width=True)
 
         # Make a POST request to the Flask app to get the prediction
-        response = requests.post(f'http://127.0.0.1:5000/predict/')
+        response = requests.post(f'https://zacam.pythonanywhere.com/predict/')
 
         # Check if the request was successful
         if response.status_code == 200:
@@ -40,11 +40,11 @@ if st.button('Submit'):
             col3, col4 = st.columns(2)
 
             # Display the predicted mask and combined image
-            response = requests.get(f'http://127.0.0.1:5000/static/data/predict/mask_predicted.png')
+            response = requests.get(f'https://zacam.pythonanywhere.com/static/data/predict/mask_predicted.png')
             image = Image.open(io.BytesIO(response.content))
             col3.image(image, caption='Predicted Mask', use_column_width=True)
 
-            response = requests.get(f'http://127.0.0.1:5000/static/data/predict/combined.png')
+            response = requests.get(f'https://zacam.pythonanywhere.com/static/data/predict/combined.png')
             image = Image.open(io.BytesIO(response.content))
             col4.image(image, caption='Combined Image', use_column_width=True)
         else:
